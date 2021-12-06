2378266fb9e54b48f652ed9716972495ce8a19d6
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-06 06:44:57 +0100

    function

--- src/Routes/products/prod.js
+++ src/Routes/products/prod.js
@@ -1,9 +1,28 @@
 import express from 'express'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import fs from 'fs'
+
 
 const prodRouter = express.Router()
 
+
+const folderPath = dirname(dirname(dirname(fileURLToPath(
+    import.meta.url))))
+
+const pathProdJson = join(folderPath, './data/products.json')
+
+const getProd = () => {
+    JSON.parse(fs.readFileSync(pathProdJson))
+}
+const writeProd = (content) => {
+    fs.writeFileSync(pathProdJson, JSON.stringify(content))
+}
+
 prodRouter.get("/", (req, res, next) => {
-    rex.send("right request")
+    const prods = getProd()
+    res.send("right request")
 })
+
 
 export default prodRouter
