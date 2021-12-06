dfd15c0fa2ef6e61270aa6f8efc859c0355ac595
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-06 08:12:18 +0100

    error handlers

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+export const genericError = (err, req, res, next) => {
+    res.status(500).send("Generic error!")
+}
+
+export const notFoundError = (err, req, res, next) => {
+    res.status(404).send("Not Found!")
+}
+
+export const genericError = (err, req, res, next) => {
+    res.status(400).send("Bad request!")
+}
--- src/server.js
+++ src/server.js
@@ -4,13 +4,15 @@ import mainRouter from './Routes/mainroute.js'
 import userRouter from './Routes/authors/authors.js'
 import productRouter from './Routes/products/products.js'
 import prodRouter from './Routes/products/prod.js'
+import cors from 'cors'
+import { genericError } from './middlewares/errorHandler.js'
 
 
 const server = express()
 const port = 3001
 
 //========= Middlewares
-
+server.use(cors())
 server.use(express.json())
 
 // ======== Endpoints
@@ -20,9 +22,9 @@ server.use("/users", userRouter)
 server.use("/products", productRouter)
 server.use("/prod", prodRouter)
 
+// ======== Errors Handeler
 
-
-
+server.use(genericError)
 console.table(listendpoints(server))
 server.listen(port, () => {
     console.log(`server is running on port ${port}`)
